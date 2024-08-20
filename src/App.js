910a1787966e6de426fdc45c4bc40a6af9fbbb55
patricio355 <patricioquiroga35@gmail.com>
2024-08-20 00:48:52 -0300

    cambie el index

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          holaaa
-          Edit <code>src/App.js</code> and save to reload.
+         esta es mi nueva pagina
+        
         </p>
         <a
           className="App-link"
